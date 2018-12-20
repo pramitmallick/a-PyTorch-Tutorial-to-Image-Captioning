fc2d52cde74e51a6b342cadc670f82fb55bec755
Pramit Mallick <pramitmallick@gmail.com>
2018-12-19 22:40:29 -0500

    sh + pat

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --verbose
+#SBATCH --job-name=captioning
+#SBATCH --time=100:00:00
+#SBATCH --nodes=1
+#SBATCH --mem=50GB
+###SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+
+python train.py
