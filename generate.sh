f68d12f6a4b22f59d509e5366f7939870eb8dc2b
Pramit -prince <pramitmallick@gmail.com>
2018-12-20 16:02:59 -0500

    pseudo-parallel corpus generated

--- generate.sh
+++ generate.sh
@@ -7,5 +7,5 @@
 ###SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 
-python -u generate.py --model='../best_model/BEST_checkpoint_coco_5_cap_per_img_5_min_word_freq.pth.tar' --word_map='../best_model/WORDMAP_coco_5_cap_per_img_5_min_word_freq.json' --beam_size=5
+python -u generate.py --model='../best_model/BEST_checkpoint_coco_5_cap_per_img_5_min_word_freq.pth.tar' --word_map='../best_model/WORDMAP_coco_5_cap_per_img_5_min_word_freq.json' --beam_size=10
 #python -u train.py
